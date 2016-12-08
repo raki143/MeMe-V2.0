77fda79b160b0ed47e3794d43e93b8f39d0b8c14
Rakesh Kumar <ace.rakesh@gmail.com>
2016-12-08 15:25:48 +0530

    implemented collection view

---
+++ MeMe V2.0/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MeMe V2.0
+//
+//  Created by Rakesh on 12/8/16.
+//  Copyright © 2016 Rakesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var collectionCellImageView: UIImageView!
+    
+}
